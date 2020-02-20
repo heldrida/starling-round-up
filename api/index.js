68d8e76b293a20e28d126efa4db6a234d79879c5
Helder Oliveira <heldrida@gmail.com>
2020-02-20 16:49:03 +0000

    include comments explaining how the reverse proxy works

--- api/index.js
+++ api/index.js
@@ -1,9 +1,20 @@
+/**
+ * The dotenv allows me to import environment variables
+ * check the README.md for instructions regarding need to set your own ACCESS_TOKEN
+ * as this is not available in the repository as it's sensitive or private data
+ * For example, in production we'd have AWS Secrets Manager or Hashicorp Vault
+ */
 require('dotenv').config()
 
 const express = require('express')
 const axios = require('axios')
 const { DEFAULT_SERVER_PORT, RESOURCES_ENDPOINT } = require('../src/Utils/constants')
+const app = express()
 
+/**
+ * Creates an reusable Axios instance that includes the required headers,
+ * for example the `Authorization` paired ACCESS TOKEN
+ */
 const axiosInstance = axios.create({
   baseURL: RESOURCES_ENDPOINT,
   headers: {
@@ -13,6 +24,11 @@ const axiosInstance = axios.create({
   timeout: 5000,
 })
 
+/**
+ * A reusable GET method that takes the original request path and query
+ * making a request to the target resources endpoint(s)
+ * @param `originalUrl` is value we pick from the clients original request
+ */
 const get = async (originalUrl) => {
   try {
     const { data } = await axiosInstance.get(originalUrl)
@@ -22,13 +38,19 @@ const get = async (originalUrl) => {
   }
 }
 
-const app = express()
-
+/**
+ * Middleware that sets header for the responses (called everytime)
+ */
 app.use((req, res, next) => {
   res.setHeader('Content-Type', 'application/json')
   next()
 })
 
+/**
+ * Listens to any request functioning as a reverse proxy service
+ * hidding the target resource endpoint location and the ACCESS_TOKEN
+ * that is passed in each request or call
+ */
 app.use('*', async (req, res) => {
   const { originalUrl } = req
   try {
